b76ebb88917a20b1f6038240f2707389152a5352
MilkMan90 <mlkaufman747@gmail.com>
2016-10-11 19:11:35 -0600

    react-setup

---
+++ app/components/SubmitButton.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+class SubmitButton extends React.Component {
+  render () {
+    return (
+      <button className="SubmitButton" onClick={this.props.handleClick}>
+        <span>{this.props.text}</span>
+      </button>
+    )
+  }
+}
+
+module.exports = SubmitButton
