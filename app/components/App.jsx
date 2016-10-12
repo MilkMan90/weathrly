59d0a95d14eb4398dfe61c68f003f2fd7fd83439
MilkMan90 <mlkaufman747@gmail.com>
2016-10-12 17:44:23 -0600

    zip code validation

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,3 +1,4 @@
+require('jquery')
 const React = require('react')
 const ReactDOM = require('react-dom')
 const LikesCounter = require('./LikesCounter')
@@ -5,45 +6,67 @@ const SubmitButton = require('./SubmitButton')
 const UserInputField = require('./UserInputField')
 const LocationInput = require('./LocationInput')
 
+
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       city: '',
       state: '',
       zip: '',
-      data: ''
+      data: '',
+      invalidInput: false
     };
     // this.props.hourly = '',
     // this.props.current = '',
   }
-  setLocation({city, state, zip}){
+  setLocation({city, state, zip}) {
     this.setState({
       city: city,
       state: state,
-      zip: zip
-    });
-    //check valid input
+      zip: zip,
+      invalidInput: false
+    }, () => {this.callZipAPI()});
+    // console.log(city, state, zip);
+    // console.log(this)
     //do api call
   }
-  checkValidInput(){
-    
+  callZipAPI() {
+    console.log(this.props.url)
+    var url = this.props.url + 'conditions/q/' + this.state.zip + '.json'
+    console.log(url)
+    $.get(url, function(data) {
+      this.setState({data:data})
+      console.log(data);
+    }.bind(this));
   }
-  callAPI(){
+  callCityAPI() {
 
+  }
+  invalidInput() {
+    this.setState({
+      invalidInput: true
+    })
   }
   render () {
     let errorMessage;
+    let invalidInputError;
     // if(true) {
     //   errorMessage = (<div>WOOT</div>)
     // }
+    if (this.state.invalidInput === true) {
+      invalidInputError = (<div>Please Enter a Valid Zip-code</div>)
+    } else {
+      invalidInputError = ''
+    }
     return (
       <div>
-        <LocationInput getLocation={this.setLocation.bind(this)}/>
+        <LocationInput getLocation={this.setLocation.bind(this)} invalidInput={this.invalidInput.bind(this)}/>
+        {invalidInputError}
         {errorMessage}
       </div>
     )
   }
 }
 
-ReactDOM.render( <App />, document.getElementById('application'))
+ReactDOM.render( <App url='http://api.wunderground.com/api/881631f063e09bd3/'/>, document.getElementById('application'))
